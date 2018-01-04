3a8cf080cbcb45c44528385ade87fc97885baea5
SiyuWang <wangsiyu.kid@bytedance.com>
2018-01-04 20:48:12 +0800

    complete score helper

--- helpers/score_helper.py
+++ helpers/score_helper.py
@@ -1,6 +1,8 @@
 import skimage.color
+import numpy as np
 
 from skimage import data
+import skimage.transform
 from skimage.filters import threshold_otsu
 from skimage.segmentation import clear_border
 from skimage.measure import label, regionprops
@@ -13,9 +15,26 @@ SCORE_AREA_R_END = 300
 SCORE_AREA_C_START = 0
 SCORE_AREA_C_END = 400
 
+NUMBER_MODEL = []
+for i in range(10):
+    fn = 'digtial/%s.npy' % i
+    mx = np.load(fn)
+    NUMBER_MODEL.append(mx)
+
+NUMBER_MODEL = np.dstack(NUMBER_MODEL)
+NUMBER_MODEL = np.rollaxis(NUMBER_MODEL, -1)
+
+
+def get_real_number(number_objects):
+    ret = ''
+    for i in number_objects:
+        ret += str(np.argmin(((i - NUMBER_MODEL) ** 2).sum(axis=1).sum(axis=-1)))
+
+    return int(ret)
+
 
 def get_score_area(game_image):
-    return game_image[SCORE_AREA_R_START:SCORE_AREA_R_END, SCORE_AREA_C_START, SCORE_AREA_C_END]
+    return game_image[SCORE_AREA_R_START:SCORE_AREA_R_END, SCORE_AREA_C_START: SCORE_AREA_C_END]
 
 
 def _get_score_object(score_area):
@@ -34,14 +53,20 @@ def _get_score_object(score_area):
         regions.append((r_min, c_min, r_max, y_max))
     regions = sorted(regions, key=lambda x: x[1])
 
-    ret_number = []
+    ret_numbers = []
     for r in regions:
         number_image = gray_image[r[0]:r[2], r[1]:r[3]]
-        ret_number.append(number_image)
+        number_image = skimage.transform.resize(number_image, (83, 67))
+        ret_numbers.append(number_image)
 
-    return ret_number
+    return ret_numbers
 
 
 def get_score_object(game_image):
     score_area = get_score_area(game_image)
     return _get_score_object(score_area)
+
+
+def get_game_score(game_image):
+    number_object = get_score_object(game_image)
+    return get_real_number(number_object)
